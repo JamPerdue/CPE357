02c419f74fbe93a4525ab7b240cc58ebf3fa456a
JamPerdue <perdue@calpoly.edu>
2019-04-08 17:42:55 -0700

    MyTr trans almost working

--- Assignments/HW1/mytr.c
+++ Assignments/HW1/mytr.c
@@ -7,6 +7,8 @@
 void translate(char *orig, char *soln, char translate[]){
 	char last = soln[strlen(soln)-1];
 	int i = 0;
+	int check = 0;
+	char current;
 	int len1;
 	int len2;
 	len1 = strlen(orig);
@@ -14,13 +16,23 @@ void translate(char *orig, char *soln, char translate[]){
 
 	while(i<len1){
 		if(i<len2){
+			current = soln[i];	
+			translate[i] = soln[i];
 			
-			translate[i]=soln[i];
 		}
 		else{
-			
+			current = last;
 			translate[i] = last;
 		}
+		
+		check = 0;
+		while(check<i){
+			if(orig[i] == orig[check]){
+				printf("duplicate\n");
+				translate[check] = current;
+			}
+			check++;
+		}	
 		i++;
 	}
 
@@ -30,14 +42,18 @@ int main(int argc, char *argv[]){
 	char *key;
 	char *sol;
 	char translated[101];
+	char *final;
+
 	if(1<0){
 		return 1;
 	}
+
 	key = argv[1];
 	sol = argv[2];
-	/*translated = (char *)malloc(sizeof(char)*strlen(key));*/
+
 	translate(key,sol,translated);
-	printf("%s\n",translated);	
+	final = translated;
+	printf("%s\n",final);	
 	
 	return 0;
 	}
